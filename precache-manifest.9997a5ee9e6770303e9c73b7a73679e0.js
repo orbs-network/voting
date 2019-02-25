969f0c38936e5f2e7b81d8eaaa901ec024ac2638
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-02-25 10:52:53 +0200

    more to create-react-app

---
+++ precache-manifest.9997a5ee9e6770303e9c73b7a73679e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ca140e46c8edc3478a7",
+    "url": "/static/css/main.11ddd15d.chunk.css"
+  },
+  {
+    "revision": "3ca140e46c8edc3478a7",
+    "url": "/static/js/main.3ca140e4.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "eee7cd07e27098a78091",
+    "url": "/static/js/2.eee7cd07.chunk.js"
+  },
+  {
+    "revision": "11244fd9b2f4cd74029f2643d484ca22",
+    "url": "/index.html"
+  }
+];
