2cade482d9e2a5e48c097b0ba6c6a90fdbc17c7b
Or Lavy <orlavyil@gmail.com>
2020-04-05 16:57:04 +0300

    deploy

---
+++ precache-manifest.35920838f556cef7594692dfeb9c1412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2616a596d181ff516cfc6d713a113c",
+    "url": "/voting/index.html"
+  },
+  {
+    "revision": "1e95612bf1084c3a87f8",
+    "url": "/voting/static/js/2.4783acab.chunk.js"
+  },
+  {
+    "revision": "25307f2155b3a29585ee34990d2ac40c",
+    "url": "/voting/static/js/2.4783acab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4958c94774346931d169",
+    "url": "/voting/static/js/main.f177bcf1.chunk.js"
+  },
+  {
+    "revision": "cb1ee1f8eb0bd1a77503",
+    "url": "/voting/static/js/runtime-main.3bbe7d40.js"
+  },
+  {
+    "revision": "e59b3ac907386fd04f4f3ad39813cfc2",
+    "url": "/voting/static/media/logo-white.e59b3ac9.svg"
+  }
+]);
